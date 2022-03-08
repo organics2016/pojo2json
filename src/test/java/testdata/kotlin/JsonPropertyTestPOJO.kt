bed9e6d4cc106332c7826b76c9c8e92d39516cdb
organics <organics2015@outlook.com>
2022-03-08 22:54:12 +0800

    release 1.1.7

--- src/test/java/testdata/kotlin/JsonPropertyTestPOJO.kt
+++ src/test/java/testdata/kotlin/JsonPropertyTestPOJO.kt
@@ -1,10 +1,16 @@
 package testdata.kotlin
 
+import com.alibaba.fastjson.annotation.JSONField
+import com.fasterxml.jackson.annotation.JsonProperty
+
 class JsonPropertyTestPOJO {
-    // IDEA 测试用例下好像不支持 import 第三方资源包
-    @com.fasterxml.jackson.annotation.JsonProperty("name")
+
+    @JsonProperty
+    private val userId: String? = null
+
+    @JsonProperty("name")
     private val username: String? = null
 
-    @com.alibaba.fastjson.annotation.JSONField(name = "pass")
+    @JSONField(name = "pass")
     private val password: String? = null
 }
