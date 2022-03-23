ccfa1f94f570845c1d80a00aa0ad25354a631410
organics <organics2015@outlook.com>
2022-03-23 15:31:11 +0800

    update

--- src/main/java/ink/organics/pojo2json/parser/RandomPOJO2JSONParser.java
+++ src/main/java/ink/organics/pojo2json/parser/RandomPOJO2JSONParser.java
@@ -4,7 +4,6 @@ import ink.organics.pojo2json.parser.type.SpecifyType;
 
 public class RandomPOJO2JSONParser extends POJO2JSONParser {
 
-
     @Override
     protected Object getFakeValue(SpecifyType specifyType) {
         return specifyType.random();
